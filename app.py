725521ba47081332f92d62f7bd3e48dc478f09c9
andreituta <tuta.andrei96@gmail.com>
2021-03-25 20:47:40 +0000

    Add error handler and path to uploaded files. Change to multi prop

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory
+from werkzeug.utils import secure_filename
 ### Utils ###
 import configparser
 
@@ -17,17 +19,33 @@ print(f'Session cookie secured: {app.config["SESSION_COOKIE_SECURE"]}')
 
 
 
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = app.config["IMAGE_UPLOADS"]
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
 
 @app.route('/', methods=['POST'])
-def upload_file():
+def upload_files():
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(list({app.config["IMAGE_UPLOADS"]})[0] + uploaded_file.filename)
-    return redirect(url_for('index'))
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            return "Invalid image", 400
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return '', 204
 
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
